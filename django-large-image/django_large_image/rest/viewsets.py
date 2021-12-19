b882522208c9967d6bdc89f5121acd50bd44f20a
banesullivan <bane.sullivan@kitware.com>
2021-12-19 03:02:26 -0700

    Cleanup base viewsets

--- django-large-image/django_large_image/rest/viewsets.py
+++ django-large-image/django_large_image/rest/viewsets.py
@@ -3,18 +3,14 @@ from rest_framework import mixins, viewsets
 
 
 class ModelViewSet(
-    # mixins.CreateModelMixin,
     mixins.RetrieveModelMixin,
-    # mixins.UpdateModelMixin,
-    # mixins.DestroyModelMixin,
     mixins.ListModelMixin,
     viewsets.GenericViewSet,
 ):
     """
     A viewset for read and edit models.
 
-    Provides default `create()`, `retrieve()`, `update()`,
-    `partial_update()`, `destroy()` and `list()` actions.
+    Provides default `retrieve()` and `list()` actions.
     """
 
     pass
@@ -28,13 +24,3 @@ class ImageSetViewSet(ModelViewSet):
 class ImageViewSet(ModelViewSet):
     serializer_class = serializers.ImageSerializer
     queryset = models.Image.objects.all()
-
-    # @swagger_auto_schema(
-    #     method='GET',
-    #     operation_summary='Download the associated Image data for this Image directly from S3.',
-    # )
-    # @action(detail=True)
-    # def data(self, *args, **kwargs):
-    #     obj = self.get_object()
-    #     url = obj.file.get_url()
-    #     return HttpResponseRedirect(url)
