748d04b7ff63ad48ffdd477956e30f7d90b85eee
banesullivan <bane.sullivan@kitware.com>
2021-12-19 02:25:34 -0700

    Disable celery

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 release: ./manage.py migrate
 web: gunicorn --bind 0.0.0.0:$PORT example.wsgi
-worker: REMAP_SIGTERM=SIGQUIT celery --app example.celery worker --loglevel INFO --without-heartbeat
--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'celery',
         'django>=3.2',
         'django-allauth',
         'django-configurations[database,email]',
