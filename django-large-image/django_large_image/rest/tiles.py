d1afabfc29d298e2bd628d5b0dbb5af5e8732329
banesullivan <bane.sullivan@kitware.com>
2022-03-22 14:27:50 -0600

    Linting

--- django-large-image/django_large_image/rest/tiles.py
+++ django-large-image/django_large_image/rest/tiles.py
@@ -11,7 +11,6 @@ from drf_yasg.utils import swagger_auto_schema
 import large_image
 from large_image.tilesource import FileTileSource
 from rest_framework.decorators import action
-from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -19,22 +18,22 @@ from rest_framework.views import APIView
 CACHE_TIMEOUT = 60 * 60 * 2
 
 z_param = openapi.Parameter(
-    'z', openapi.IN_PATH, description="zoom level", type=openapi.TYPE_INTEGER
+    'z', openapi.IN_PATH, description='zoom level', type=openapi.TYPE_INTEGER
 )
-x_param = openapi.Parameter('x', openapi.IN_PATH, description="x", type=openapi.TYPE_INTEGER)
-y_param = openapi.Parameter('y', openapi.IN_PATH, description="y", type=openapi.TYPE_INTEGER)
+x_param = openapi.Parameter('x', openapi.IN_PATH, description='x', type=openapi.TYPE_INTEGER)
+y_param = openapi.Parameter('y', openapi.IN_PATH, description='y', type=openapi.TYPE_INTEGER)
 band_param = openapi.Parameter(
-    'band', openapi.IN_PATH, description="band index", type=openapi.TYPE_INTEGER
+    'band', openapi.IN_PATH, description='band index', type=openapi.TYPE_INTEGER
 )
 left_param = openapi.Parameter(
-    'left', openapi.IN_PATH, description="left", type=openapi.TYPE_NUMBER
+    'left', openapi.IN_PATH, description='left', type=openapi.TYPE_NUMBER
 )
 right_param = openapi.Parameter(
-    'right', openapi.IN_PATH, description="right", type=openapi.TYPE_NUMBER
+    'right', openapi.IN_PATH, description='right', type=openapi.TYPE_NUMBER
 )
-top_param = openapi.Parameter('top', openapi.IN_PATH, description="top", type=openapi.TYPE_NUMBER)
+top_param = openapi.Parameter('top', openapi.IN_PATH, description='top', type=openapi.TYPE_NUMBER)
 bottom_param = openapi.Parameter(
-    'bottom', openapi.IN_PATH, description="bottom", type=openapi.TYPE_NUMBER
+    'bottom', openapi.IN_PATH, description='bottom', type=openapi.TYPE_NUMBER
 )
 
 
@@ -199,7 +198,7 @@ class LargeImageView(APIView):
     def region(
         self, request: Request, pk: int, left: float, right: float, bottom: float, top: float
     ) -> HttpResponse:
-        """Returns region tile binary from world coordinates in given EPSG.
+        """Return the region tile binary from world coordinates in given EPSG.
 
         Note
         ----
