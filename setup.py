279a9893f64051ce44ac937724375f31b57bb820
banesullivan <bane.sullivan@kitware.com>
2022-04-19 20:11:26 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ else:
 
 setup(
     name='django-large-image',
-    version='0.4.3',
+    version='0.4.4',
     description='',
     long_description=long_description,
     long_description_content_type='text/markdown',
