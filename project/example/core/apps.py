13273f5eabf422ce657df58a0997045ceb62d3e9
banesullivan <bane.sullivan@kitware.com>
2022-04-26 17:30:43 -0600

    Better logging on production

--- project/example/core/apps.py
+++ project/example/core/apps.py
@@ -1,6 +1,17 @@
+import logging
+
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class CoreConfig(AppConfig):
     name = 'example.core'
     verbose_name = 'Django Large Image: Core'
+
+    def ready(self):
+        if not getattr(settings, 'DEBUG', False):
+            logging.getLogger("gdal").setLevel(logging.ERROR)
+            logging.getLogger("large_image").setLevel(logging.ERROR)
+            logging.getLogger("tifftools").setLevel(logging.ERROR)
+            logging.getLogger("pyvips").setLevel(logging.ERROR)
+            logging.getLogger("PIL").setLevel(logging.ERROR)
