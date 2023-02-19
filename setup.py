94915469e58e3d9c987c0d7743719686507f0017
Bane Sullivan <bane.sullivan@kitware.com>
2023-02-19 15:52:41 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ else:
 
 setup(
     name='django-large-image',
-    version='0.8.1',
+    version='0.8.2',
     description='Dynamic tile server in Django built on top of large-image (and GDAL)',
     long_description=long_description,
     long_description_content_type='text/markdown',
