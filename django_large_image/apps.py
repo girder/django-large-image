a8ad0dada94db320b347bfe827598ffe1744974a
Anne Haley <anne.haley@kitware.com>
2026-03-20 12:32:41 -0400

    Allow additional settings to adjust large image cache config

--- django_large_image/apps.py
+++ django_large_image/apps.py
@@ -1,6 +1,7 @@
 import logging
 
 from django.apps import AppConfig
+from django.conf import settings
 import large_image
 
 logger = logging.getLogger(__name__)
@@ -13,6 +14,27 @@ class DjangoLargeImageConfig(AppConfig):
 
     def ready(self):
         # Set up cache with large_image
-        # This isn't necessary but it makes sure we always default
-        #   to the django cache if others are available
-        large_image.config.setConfig('cache_backend', 'django')
+        # Use any existing settings defined by other installed apps,
+        # Defaulting to caching with django
+        large_image.config.setConfig(
+            'cache_backend',
+            getattr(settings, 'LARGE_IMAGE_CACHE_BACKEND', 'django'),
+        )
+
+        # Set any other cache config if settings are defined
+        for config_var_name in [
+            'cache_python_memory_portion',
+            'cache_memcached_url',
+            'cache_memcached_username',
+            'cache_memcached_password',
+            'cache_redis_url',
+            'cache_redis_username',
+            'cache_redis_password',
+            'cache_tilesource_memory_portion',
+            'cache_tilesource_maximum',
+            'cache_sources',
+        ]:
+            settings_attr_name = 'LARGE_IMAGE_' + config_var_name.upper()
+            settings_attr_value = getattr(settings, settings_attr_name)
+            if settings_attr_value is not None:
+                large_image.config.setConfig(config_var_name, settings_attr_value)
